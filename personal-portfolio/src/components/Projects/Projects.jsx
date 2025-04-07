6b457811f518ea70eb4608074836f781f5e9e362
gauravnainwal229 <gauravnainwal229@gmail.com>
2025-04-07 21:27:38 +0530

    adding live link in first project

--- personal-portfolio/src/components/Projects/Projects.jsx
+++ personal-portfolio/src/components/Projects/Projects.jsx
@@ -10,7 +10,7 @@ const Projects = () => {
         "📋 Manages patient records digitally.\n📅 Allows easy online appointment booking.\n🔐 Ensures secure and smooth access for doctors and patients.",
       image: "/images/project1.png",
       github: "https://github.com/surajbisht-dev/Online_Healthcare_Checkup",
-      live: "https://liveproject1.com",
+      live: "https://online-healthcare-checkup-frontend.onrender.com",
     },
     {
       title: "Tourism Platform",
