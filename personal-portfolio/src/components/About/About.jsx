0cab2b1afd06c2acaef9d2dbf96f90294c4c610d
gauravnainwal229 <gauravnainwal229@gmail.com>
2025-04-10 21:34:20 +0530

    resume link updated

--- personal-portfolio/src/components/About/About.jsx
+++ personal-portfolio/src/components/About/About.jsx
@@ -67,7 +67,7 @@ const About = () => {
           variants={childVariants}
         >
           <a
-            href="#resume"
+            href="https://drive.google.com/file/d/15-y0IQEOWBp3gwMX1KHC0w9ot1LRvEpk/view?usp=drive_link"
             className="px-6 py-2 border-2 border-white text-white font-medium hover:bg-blue-500 hover:border-blue-500 transition-all duration-300 rounded-md"
           >
             View Resume
